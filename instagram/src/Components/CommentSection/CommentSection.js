0c7fd20e54b9de6fc0bf499d40e6a5ecd0d511e9
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-02-18 21:44:18 -0500

    Layout is mobile and dekstop responsive

--- instagram/src/Components/CommentSection/CommentSection.js
+++ instagram/src/Components/CommentSection/CommentSection.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Comment from './Comment';
-
+import './Comment.css';
 class CommentSection extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +10,7 @@ class CommentSection extends React.Component {
   }
   render() {
     return (
-      <div>
+      <div className= 'comment-sec'>
         {this.state.comments.map( (c, i) => <Comment key={i} user={c.username} text={c.text} /> )}
       </div>
     );
